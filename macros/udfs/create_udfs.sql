3e6f91af50509672ae1ea9bac05f1fe4f7d87e18
Stacie Brown <stacie.brown@dbtlabs.com>
2025-11-03 19:35:24 +0000

    After running “dbt run -s dim_customers”, seeing the following error:  [UNSUPPORTED_DATATYPE] Unsupported data type "NUMBER(38,0)". SQLSTATE: 0A000 == SQL (line 24, position 18) == cast(null as number(38,0))

---
+++ macros/udfs/create_udfs.sql
@@ -0,0 +1,1 @@
+{% macro create_udfs() %} {% do run_query(create_area_of_circle()) %} {% endmacro %}
---
+++ macros/udfs/unit_test_mode.sql
@@ -0,0 +1,11 @@
+{% macro unit_test_mode() %}
+
+    {% if var is not defined %} {{ return(False) }}
+
+    {% elif var("test") == "true" %} {{ return(True) }}
+
+    {% else %} {{ return(False) }}
+
+    {% endif %}
+
+{% endmacro %}
